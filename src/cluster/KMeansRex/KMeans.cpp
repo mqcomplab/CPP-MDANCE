be088a75acd9318b63a0e68baa934eddef1ceed2
vicciv1623 <vickyhaeunjung@gmail.com>
2025-12-06 13:25:22 -0500

    fix: rid magic numbers

--- src/cluster/KMeansRex/KMeans.cpp
+++ src/cluster/KMeansRex/KMeans.cpp
@@ -35,6 +35,9 @@ Date:   2 April 2013
 #include "../../tools/bts.h"
 #include "../../tools/scores.h"
 
+constexpr double EPSILON_DIV = 1e-100;
+constexpr int VECTORIZATION_THRESHOLD = 16;
+
 class KmeansNANI{
     Mat data;
     Mat centers;
@@ -201,7 +204,7 @@ class KmeansNANI{
 
         // For small dims D, for loop is noticeably faster than fully vectorized.
         // Odd but true.  So we do fastest thing 
-        if ( D <= 16 ) {
+        if ( D <= VECTORIZATION_THRESHOLD ) {
             for (int kk=0; kk<K; kk++) {
                 Dist.col(kk) = (data.rowwise() - centers.row(kk)).square().rowwise().sum();
             }    
@@ -233,7 +236,7 @@ class KmeansNANI{
             centers.row((int) labels(nn,0)) += data.row(nn);
             NperCluster[(int) labels(nn,0)] += 1;
         }  
-        NperCluster += 1e-100; // avoid division-by-zero
+        NperCluster += EPSILON_DIV; // avoid division-by-zero
         for (int k=0; k < centers.rows(); k++) {
         centers.row(k) /= NperCluster(k);
         }
