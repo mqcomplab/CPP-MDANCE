f3157ffc34ea6d378c265818084b9c82497e2d9f
vicciv1623 <vickyhaeunjung@gmail.com>
2025-12-18 16:15:43 -0500

    add testClusn

--- tests/test_helm.cpp
+++ tests/test_helm.cpp
@@ -4,6 +4,7 @@
 #include "test_utils.h"
 
 #include "../../src/cluster/helm.cpp"
+#include "../../tools/scores.cpp"
 
 TestHelm::TestHelm() {
     // read in data:
@@ -22,10 +23,11 @@ TestHelm::TestHelm() {
     inputCluster();
 }
 
- vector<Cluster> TestHelm::inputCluster(){
+ void TestHelm::inputCluster(){
     for (int i = 0; i < labels.size(); i++) {
         uniqueLabels.insert(labels(i));
     }
+
     std::vector<Cluster> clusters;
     int N0 = uniqueLabels.size();
     for ( int i = 0; i < N0; i++) {
@@ -47,8 +49,6 @@ TestHelm::TestHelm() {
         throw std::runtime_error("Error in inputCluster: number of unique labels does not match number of clusters.");
     }
     clusters_map[N0] = clusters;
-
-    return clusters;
 }
 
 TEST_F(TestHelm, TestPops){
@@ -96,11 +96,60 @@ TEST_F(TestHelm, TestPops){
 
 TEST_F(TestHelm, TestClus){
     int N0 = uniqueLabels.size();
-    vector<Cluster> clusters = inputCluster();
+    inputCluster();
     int nAtoms = 50; 
     int nClusters = 37;
     Helm helm = Helm(clusters_map, nAtoms, MD::Metric::MSD, MD::MergeScheme::Inter, nClusters);
     map<int, vector<Cluster>> res = helm.run();
 
-    
+    vector<pair<double, double>> scores;
+    for(auto it=res.begin(); it!=res.end(); it++){
+        vector<int> idx;
+        for(auto c:it->second){
+            idx.insert(idx.end(), c.getIndices().begin(), c.getIndices().end());
+        }
+
+        vector<int> temp;
+        for(int i:idx){
+            for(int j=0; j<labels.size(); j++){
+                if(labels(j)==i){
+                    temp.emplace_back(j);
+                }
+            }
+        }
+        Mat arr = data(temp, Eigen::placeholders::all);
+        scores.emplace_back(helm.computeScores(it->second, arr));
+    }
+
+    vector<pair<double, double>> expectedScores = {
+        {291.2198060306322, 1.7370614645545726},
+        {295.7352641684398, 1.7122884537735075}, 
+        {58296.11490509768595, 1.7245038612367665}, 
+        {297.8213492701506, 1.7246552370601154}, 
+        {299.0810730592307, 1.738637643465005}, 
+        {300.34386300863565, 1.750692719498292}, 
+        {302.7012989347063, 1.755325543510106}, 
+        {304.51797241739484, 1.7672459242576242}, 
+        {306.6006824661377, 1.7695122974000195}, 
+        {308.9021982976074, 1.7607190308607732}, 
+        {307.1901532394585, 1.7721711289472055}, 
+        {297.43176362926556, 1.7888202076551794}, 
+        {299.34757173879535, 1.7833018738518591}, 
+        {301.52432336513584, 1.7936056164868994}, 
+        {306.08279103249237, 1.8034172355518991}, 
+        {310.3208501789975, 1.809472686067903}, 
+        {310.0361137593372, 1.825336725435764}, 
+        {313.8978629372179, 1.8344594141834631}, 
+        {315.0527200319327, 1.828991467839653}, 
+        {314.5428710018461, 1.818951502602474}, 
+        {314.94309021259465, 1.8091147842592137}, 
+        {318.77039278363225, 1.8100164274989112}, 
+        {323.7748207939142, 1.817475357075402}, 
+        {329.3068227654963, 1.8437066236912167}
+    };
+
+    for(int i=0; i<scores.size(); i++){
+        EXPECT_NEAR(scores[i].first, expectedScores[i].first, 1e-5);
+        EXPECT_NEAR(scores[i].second, expectedScores[i].second, 1e-5);
+    }
 }
