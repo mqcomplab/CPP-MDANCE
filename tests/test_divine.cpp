997860dcda13efc3cab31ae5c89f06a583e362ef
vicciv1623 <vickyhaeunjung@gmail.com>
2026-01-15 13:54:47 -0500

    add testCombination

--- tests/test_divine.cpp
+++ tests/test_divine.cpp
@@ -14,6 +14,7 @@ DivineTest::DivineTest(){
 }
 
 TEST_F(DivineTest, TestThreshold){
+    //std::cout<<data.row(1)<<std::endl;
     Divine model=Divine(data, MD::DivineSplit::WeightedMSD, MD::DivineAnchors::NANI, 
                         MD::KinitType::CompSim, 0, 3, true, 1, 0.2);
     labels = model.getLabels();
@@ -21,5 +22,56 @@ TEST_F(DivineTest, TestThreshold){
 
     for(auto c:clusters){
         ASSERT_GE(c.size(), 30);
+    }
+}
+
+TEST_F(DivineTest, TestCombinations){
+    vector<MD::DivineSplit> split={
+        MD::DivineSplit::MSD,
+        MD::DivineSplit::Radius,
+        MD::DivineSplit::WeightedMSD
+    };
+    vector<MD::DivineAnchors> anchor={
+        MD::DivineAnchors::NANI,
+        MD::DivineAnchors::OutlierPair,
+        MD::DivineAnchors::SplinterPair
+    };
+    vector<bool> refine={
+        true,
+        false
+    };
+
+    int nAtoms = 1;
+    int end = 0;        //end = 0 means 'k'
+    for(auto s:split){
+        for(auto a:anchor){
+            for(auto r:refine){
+                Divine model=Divine(
+                    data,
+                    s,
+                    a,
+                    MD::KinitType::CompSim,
+                    end,
+                    3,
+                    r,
+                    nAtoms
+                );
+                labels = model.getLabels();
+                clusters = model.getClusters();
+                scores = model.getScores();
+
+                set<int> uniqueLabels;
+                for(auto i:labels){
+                    uniqueLabels.insert(i);
+                }
+                
+                //assert #1
+                //asserst #2
+                EXPECT_EQ(clusters.size(), 3);
+                //assert isinstance
+                EXPECT_EQ(labels.size(), data.rows());
+                EXPECT_EQ(uniqueLabels.size(), 3);
+            }
+        }
     }
 }
