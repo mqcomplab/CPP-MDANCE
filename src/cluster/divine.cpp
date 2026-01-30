e6a2494b790438d5be52c4650100358a9866d098
vicciv1623 <vickyhaeunjung@gmail.com>
2026-01-30 15:37:22 -0500

    get rid of oudated message

--- src/cluster/divine.cpp
+++ src/cluster/divine.cpp
@@ -22,7 +22,6 @@ void Divine::divisiveAlgorithm() {
             Index clusterToSplit = selectClusterToSplit(failedSplits);
             if (clusterToSplit < 0) {
                 throw std::runtime_error("No more cluster splits possible that would yield valid subclusters");
-                //std::cerr<<"Cluster"<<clusterToSplit<<"could not be split meaningfully — skipping."<<std::endl;
             } 
             didSplit = splitCluster(clusterToSplit, minFrames);
             failedSplits[clusterToSplit] = !didSplit;
