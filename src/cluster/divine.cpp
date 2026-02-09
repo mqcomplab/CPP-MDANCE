49bc79d8418d63db2c51860b2ecee5bed2c7f249
Krisztina Zsigmond <144738961+kzsigmond@users.noreply.github.com>
2026-02-09 16:05:53 -0500

    fix: map splinter index in splinter group

--- src/cluster/divine.cpp
+++ src/cluster/divine.cpp
@@ -156,13 +156,15 @@ bool Divine::splitCluster(Index clusterToSplit, int minFrames) {
         Index medoidIdx = calculateMedoid(subdata, nAtoms, mt);
         Vec medoidPoint = subdata.row(medoidIdx);
 
-        vector<Index> splinterGroup = {splinterIdx};
+        vector<Index> splinterGroup = {subdataIndices[splinterIdx]};
         vector<Index> mainGroup;
         splinterGroup.reserve(subdata.rows() - 1);
         mainGroup.reserve(subdata.rows() - 1);
 
         for (Index i = 0; i < subdata.rows(); ++i) {
-            if (i == splinterIdx) continue;
+            if (i == splinterIdx){
+                continue;
+            }
 
             double dS = (subdata.row(i).transpose() - splinterPoint).square().sum() / nAtoms;
             double dM = (subdata.row(i).transpose() - medoidPoint).square().sum() / nAtoms;
